18127a4ffcad4cffc76cf29e525d4d1a37ea68f4
lovewinelid <love.winelid@elev.ga.ntig.se>
2020-09-29 10:00:29 +0200

    Init.

---
+++ Anteckningar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Anteckningar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
